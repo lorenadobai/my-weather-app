5408b20d3d61c0a44ace3ea4bd8ff0c6fce0d76a
Dobai Lorena <lorena_dobai@yahoo.com>
2024-05-22 20:52:34 +0300

    prob gramaticale

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => (
   <div>
     <h1>Welcome to My Weather App</h1>
     <img src={homeImage} alt="Home" className="page-image" />
-    <p>IOTCA project for monitoring temperature and humidity data in real-time, using RaspberryPi with a temperature and humidity sensor, where the lightbulb is on in different intervals considering the levels of temperature.We have it conected fo our firebase where the data for charts is going to come.</p>
+    <p>IOTCA project for monitoring temperature and humidity data in real-time, using RaspberryPi with a temperature and humidity sensor, where the lightbulb is on in different intervals considering the levels of temperature.We have it conected to our firebase where the data for charts is going to come.</p>
   </div>
 );
 
